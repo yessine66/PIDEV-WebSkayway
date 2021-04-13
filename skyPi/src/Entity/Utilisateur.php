83508d7f1350d42f85ceabe69bb2bfef6c255aaa
yessine66 <47737513+yessine66@users.noreply.github.com>
2021-04-13 13:05:53 +0100

    crud-vierge
    no design

--- skyPi/src/Entity/Utilisateur.php
+++ skyPi/src/Entity/Utilisateur.php
@@ -98,5 +98,197 @@ class Utilisateur
      */
     private $creCompte;
 
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param string|null $nom
+     */
+    public function setNom(?string $nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param string|null $prenom
+     */
+    public function setPrenom(?string $prenom): void
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    /**
+     * @param string|null $mail
+     */
+    public function setMail(?string $mail): void
+    {
+        $this->mail = $mail;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param int|null $age
+     */
+    public function setAge(?int $age): void
+    {
+        $this->age = $age;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getTel(): ?int
+    {
+        return $this->tel;
+    }
+
+    /**
+     * @param int|null $tel
+     */
+    public function setTel(?int $tel): void
+    {
+        $this->tel = $tel;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getGenre(): ?string
+    {
+        return $this->genre;
+    }
+
+    /**
+     * @param string|null $genre
+     */
+    public function setGenre(?string $genre): void
+    {
+        $this->genre = $genre;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getDateNaiss(): ?\DateTime
+    {
+        return $this->dateNaiss;
+    }
+
+    /**
+     * @param \DateTime|null $dateNaiss
+     */
+    public function setDateNaiss(?\DateTime $dateNaiss): void
+    {
+        $this->dateNaiss = $dateNaiss;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string|null $username
+     */
+    public function setUsername(?string $username): void
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string|null $password
+     */
+    public function setPassword(?string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param string|null $role
+     */
+    public function setRole(?string $role): void
+    {
+        $this->role = $role;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getCreCompte(): ?\DateTime
+    {
+        return $this->creCompte;
+    }
+
+    /**
+     * @param \DateTime|null $creCompte
+     */
+    public function setCreCompte(?\DateTime $creCompte): void
+    {
+        $this->creCompte = $creCompte;
+    }
+
 
 }
