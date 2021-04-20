a145aa3f0dca45eb1b806f443f8fc7796ad1ac92
Fatma Abrougui <fatma.abrougui@esprit.tn>
2021-04-20 02:39:55 +0100

    -

--- src/Form/QuestionType.php
+++ src/Form/QuestionType.php
@@ -9,7 +9,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-
+use Gregwar\CaptchaBundle\Type\CaptchaType;
 class QuestionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -26,6 +26,7 @@ class QuestionType extends AbstractType
                     'ART' => 'ART',
                 ],
             ])
+            ->add('captcha', CaptchaType::class);
 
 
         ;
