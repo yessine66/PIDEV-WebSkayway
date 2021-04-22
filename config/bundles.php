4374275a79879d083b4e8eb62379441201ed3072
Fatma Abrougui <fatma.abrougui@esprit.tn>
2021-04-22 03:50:46 +0100

    -

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,4 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Gregwar\CaptchaBundle\GregwarCaptchaBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,10 +4,14 @@ namespace App\Controller;
 
 use App\Entity\Question;
 use App\Form\QuestionType;
+use App\Entity\Reponse;
+use App\Form\ReponseType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 /**
  * @Route("/question")
@@ -24,6 +28,32 @@ class QuestionController extends AbstractController
             ->findAll();
 
         return $this->render('question/index.html.twig', [
+            'questions' => $questions,
+        ]);
+    }
+    /**
+     * @Route("/index", name="questionF_index", methods={"GET"})
+     */
+    public function indexF(): Response
+    {
+        $questions = $this->getDoctrine()
+            ->getRepository(Question::class)
+            ->findAll();
+
+        return $this->render('question/indexF.html.twig', [
+            'questions' => $questions,
+        ]);
+    }
+    /**
+     * @Route("/index1", name="questionF_show", methods={"GET"})
+     */
+    public function indexS(): Response
+    {
+        $questions = $this->getDoctrine()
+            ->getRepository(Question::class)
+            ->findAll();
+
+        return $this->render('question/showF.html.twig', [
             'questions' => $questions,
         ]);
     }
@@ -114,9 +144,26 @@ class QuestionController extends AbstractController
 
     }
 
+    /**
+     * @Route("/TriCat/", name="question_cat", methods={"POST"})
+     */
+    public function FindByCategorie(EntityManagerInterface $em,Request $request): Response
+    {
+        $data=$request->get('myText');
+        $queryBuilder = $em->getRepository(Question::class)->createQueryBuilder('q');
+        $queryBuilder->andWhere('q.nameT = :cat');
+        $queryBuilder->setParameter('cat', $data);
+        $questions = $queryBuilder->getQuery()->getResult();
 
 
+        $categories = $this->getDoctrine()
+            ->getRepository(Question::class)
+            ->findAll();
 
+        return $this->render('question/showF.html.twig', [
+            'questions' => $questions,'categories' => $categories,
+        ]);
+    }
 
 
 }
