ad403a361a245cbc6a2e426de19d11bab53c91f0
yessine66 <47737513+yessine66@users.noreply.github.com>
2021-04-28 05:49:47 +0100

    registration
    ihihihihihihih

---
+++ src/Controller/EnseignantController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Enseignant;
+use App\Form\EnseignantType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/enseignant")
+ */
+class EnseignantController extends AbstractController
+{
+    /**
+     * @Route("/", name="enseignant_index", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        $enseignants = $this->getDoctrine()
+            ->getRepository(Enseignant::class)
+            ->findAll();
+
+        return $this->render('enseignant/index.html.twig', [
+            'enseignants' => $enseignants,
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="enseignant_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $enseignant = new Enseignant();
+        $form = $this->createForm(EnseignantType::class, $enseignant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($enseignant);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('enseignant_index');
+        }
+
+        return $this->render('enseignant/new.html.twig', [
+            'enseignant' => $enseignant,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{idEns}", name="enseignant_show", methods={"GET"})
+     */
+    public function show(Enseignant $enseignant): Response
+    {
+        return $this->render('enseignant/show.html.twig', [
+            'enseignant' => $enseignant,
+        ]);
+    }
+
+    /**
+     * @Route("/{idEns}/edit", name="enseignant_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Enseignant $enseignant): Response
+    {
+        $form = $this->createForm(EnseignantType::class, $enseignant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('enseignant_index');
+        }
+
+        return $this->render('enseignant/edit.html.twig', [
+            'enseignant' => $enseignant,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{idEns}", name="enseignant_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Enseignant $enseignant): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$enseignant->getIdEns(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($enseignant);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('enseignant_index');
+    }
+}
